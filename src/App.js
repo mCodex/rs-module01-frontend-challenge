da03eb392b629db0b7bf08c01cb9b8ce05ed23fe
Mateus Andrade <mat.andrade@live.com>
2020-04-26 15:18:27 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { uuid } from 'uuidv4';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
-function App() {
+export default () => {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const loadDataFromAPI = async () => {
+      const response = await api.get('/repositories');
+      setRepositories(response.data);
+    };
+
+    loadDataFromAPI();
+  }, [repositories]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      id: uuid(),
+      title: 'rs-module01-frontend-challenge',
+      url: 'https://github.com/mCodex/rs-module01-frontend-challenge',
+      techs: ['ReactJS'],
+    };
+
+    await api.post('/repositories', repository);
+
+    return setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const newRepositories = repositories.filter((r) => r.id !== id);
+
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {id} - {title}
+            <button type="button" onClick={() => handleRemoveRepository(id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button type="button" onClick={handleAddRepository}>
+        Adicionar
+      </button>
     </div>
   );
-}
-
-export default App;
+};
